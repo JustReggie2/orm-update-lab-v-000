86540008422ee87131c78029020d3725dd4eb01e
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-09-25 23:20:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,8 @@ class Student
         grade INTEGER
       );
     SQL
+
+    DB [:conn].execute(sql)
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
